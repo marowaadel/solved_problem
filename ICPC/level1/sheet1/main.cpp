719e0d2db14b99b2d736de1abc7029aebbf6bf9d
marwa <47919176+marowaadel@users.noreply.github.com>
2021-01-18 00:32:36 +0200

    Add files via upload

---
+++ ICPC/level1/sheet1/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,sump=0;
+    cin >>n;
+    for (int i=0;i<n ; i++)
+    {
+        int sum=0;
+          for (int j=0;j<3 ; j++)
+          {
+              int x;
+              cin >>x;
+              sum +=x;
+          }
+          if (sum >= 2)
+            sump ++;
+    }
+    cout << sump<< endl;
+    return 0;
+}
