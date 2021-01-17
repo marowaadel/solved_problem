5059498adc9d4499fd15a402b054757c221d7ba3
marwa <47919176+marowaadel@users.noreply.github.com>
2021-01-18 01:25:29 +0200

    Add files via upload

---
+++ ICPC/level1/sheet1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin>>w;
+    if (w == 2)
+         cout << "no" <<endl;
+    else if(w%2==0)
+        cout << "yes" <<endl;
+    else
+        cout << "no" <<endl;
+    return 0;
+}
