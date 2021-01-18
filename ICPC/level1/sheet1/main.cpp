82b170262dad74ac5886225e9a6f8ba375ac2549
marwa <47919176+marowaadel@users.noreply.github.com>
2021-01-18 17:54:54 +0200

    Update main.cpp

--- ICPC/level1/sheet1/main.cpp
+++ ICPC/level1/sheet1/main.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
-    int n ,i,sump=0,sumc=0;
+    int n ,sump=0,sumc=0;
     cin >>n;
-    int arr[n];
-    for (i=0;i<n;i++)
-        cin >>arr[i];
-    for (i=0;i<n;i++)
+    for (int i=0;i<n;i++)
     {
-        if (arr[i] >0)
-            sump +=arr[i];
+        int x;
+        cin >>x;
+        if (x >0)
+            sump +=x;
         else
         {
             if (sump >0)
