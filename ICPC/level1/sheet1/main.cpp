ba2db72b8cd93e96bb8eb5211cbaba81760a388d
marwa <47919176+marowaadel@users.noreply.github.com>
2021-01-18 17:16:30 +0200

    Add files via upload

---
+++ ICPC/level1/sheet1/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum =0;
+int main()
+{
+    string s;
+    int i,half,start;
+    cin >>s;
+    start='a';
+    for (i=0;i<s.size();i++)
+    {
+        char c=s[i];
+        int m1,m2;
+        m1=abs(c-start);  //clockwise
+        m2=abs(26-m1);   //anti clockwise
+        sum += min(m1,m2);
+        start=c;
+    }
+     cout << sum << endl;
+    return 0;
+}
