2b52c9879f1bba140bce3436ccde8ba811367175
marwa <47919176+marowaadel@users.noreply.github.com>
2021-04-18 21:43:28 +0200

    Add files via upload

---
+++ ICPC/level1/STLs_sheet3/MezoPlayingZoma.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+string s;
+//vector<int>v1,v2;
+int main()
+{
+    cin >>n>>s;
+    /*
+    for (int i=0;i<n;i++)
+    {
+        if (s[i] =='L') sml --;
+        else smr ++;
+    }
+    */
+    cout << n+1 <<endl;
+    return 0;
+}
